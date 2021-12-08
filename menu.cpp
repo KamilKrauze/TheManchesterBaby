628a6799b064aa8d7aeaee1199a68e623f343104
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2021-12-08 15:57:46 +0000

    Setting up menu

--- menu.cpp
+++ menu.cpp
@@ -1,1 +1,33 @@
 #include "menu.h"
+
+menuState menu_state = mainmenu;
+
+void mainMenu()
+{
+    cout << "\n\t=======================================" << endl;
+    cout << "\t               ASSEMBLER         " << endl;
+    cout << "\t=======================================" << endl;
+
+    cout << "\t\t1. Insert machine code" << endl;
+    cout << "\t\t2. Exit\n" << endl;
+}
+
+void machineCodeOptions()
+{
+}
+
+void determineMenuToShow()
+{
+    switch (menu_state)
+    {
+        case mainmenu:
+            mainMenu();
+            break;
+        case mcloaded:
+            //Add options
+            break;
+        case mcnotloaded:
+            //Add options
+            break;
+    }
+}
--- menu.h
+++ menu.h
@@ -1,12 +1,27 @@
 #ifndef MENU_H
     #define MENU_H
     #include <iostream>
+    #include <string>
 
+    #include "menu.h"
+    #include "system.h"
     #include "assembler.h"
 
     using namespace std;
 
+    enum menuState {
+        mainmenu,
+        mcloaded,
+        mcnotloaded,
+    };
+    
+    extern menuState menu_state;
+
     // Helper functions on displaying menu (text based)
     void mainMenu();
+    void mainMenuChoice();
+    void machineCodeOptions();
+
+    void determineMenuToShow();
     
 #endif

aa2afe86d823b10d8eb8ef292960e9613cd6e365
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2021-12-08 14:52:06 +0000

    initial commit

---
+++ menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+    #define MENU_H
+    #include <iostream>
+
+    #include "assembler.h"
+
+    using namespace std;
+
+    // Helper functions on displaying menu (text based)
+    void mainMenu();
+    
+#endif

945cee182be5ed706204b01eb2c33931306f938e
euanWest <72396471+euanWest@users.noreply.github.com>
2021-12-10 16:49:53 +0000

    Added names and Matriculation numbers to top of file.

--- ManchesterBaby.cpp
+++ ManchesterBaby.cpp
@@ -1,3 +1,11 @@
+/*
+Kamil Krauze - 2414951
+Sam Francis - 2413492
+Antoine Bonfili - 2423967
+Dmitrijs Novikovs - 2424525
+Euan West - 2420987
+*/
+
 #include <vector>
 #include <string>
 #include <iostream>
